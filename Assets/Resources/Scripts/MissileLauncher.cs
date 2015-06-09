d7989423aecd70eb6c1fea22b6553e5230e1dec1
Jaiden Mispy <jaiden@mispy.me>
2015-06-09 14:36:58 +1000

    Cleanup, start on building effect

---
+++ Assets/Resources/Scripts/MissileLauncher.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MissileLauncher : PoolBehaviour {
+	public Ship ship;
+	public Block block;
+	public Collider collider;
+	private float timeBetweenShots = 0.1f;
+	private float lastFireTime = 0f;
+
+	void OnEnable() {
+		ship = transform.parent.gameObject.GetComponent<Ship>();
+		block = ship.BlockAtWorldPos(transform.position);
+		collider = ship.colliders[block.pos].GetComponent<Collider>();
+	}
+
+	public void Fire(Vector3 worldPos) {	
+		if (Time.time - lastFireTime < timeBetweenShots)
+			return;
+
+		if (Util.TurretBlocked(ship, transform.position, worldPos, 0.3f)) {
+			return;
+		}
+
+		lastFireTime = Time.time;
+
+		var targetDir = (worldPos-transform.position).normalized;
+		var targetRotation = Quaternion.LookRotation(Vector3.forward, targetDir);
+
+		var missile = Pool.For("Missile").TakeObject();
+		missile.transform.position = transform.position;
+		missile.transform.rotation = targetRotation;
+		var mcol = missile.GetComponent<BoxCollider>();
+		var rigid = missile.GetComponent<Rigidbody>();
+		missile.SetActive(true);
+		rigid.velocity = ship.rigidBody.velocity;
+		rigid.AddForce(targetDir*0.2f);
+		Physics.IgnoreCollision(collider, mcol);
+		if (ship.shields) {
+			Physics.IgnoreCollision(ship.shields.GetComponent<Collider>(), mcol, true);
+			Physics.IgnoreCollision(mcol, ship.shields.GetComponent<Collider>(), true);
+		}
+	}
+	
+	void Update() {
+		Vector2 pz = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
+	}
+}
---
+++ Assets/Resources/Scripts/TractorBeam.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TractorBeam : MonoBehaviour {
+	public ParticleSystem beam;
+	public Ship ship;
+
+	public List<Collider> captured = new List<Collider>();
+
+	void OnEnable() {
+		ship = transform.parent.gameObject.GetComponent<Ship>();
+		beam = GetComponent<ParticleSystem>();
+		beam.enableEmission = false;
+	}
+
+	public void Stop() {
+		beam.Clear();
+		beam.enableEmission = false;
+		if (ship.shields != null) {
+			var shieldCol = ship.shields.GetComponent<SphereCollider>();
+			foreach (var col in captured) {
+				Physics.IgnoreCollision(col, shieldCol, false);
+			}
+		}
+	}
+
+	public void Fire(Vector3 worldPos) {	
+		Collider shieldCol = null;
+		if (ship.shields != null)
+			shieldCol = ship.shields.GetComponent<SphereCollider>();
+
+		foreach (var col in captured) {
+			if (col != null && col.attachedRigidbody != null) {
+				col.attachedRigidbody.drag -= 5;
+			}
+		}
+		captured.Clear();
+
+
+		if (Util.TurretBlocked(ship, transform.position, worldPos)) {
+			return;
+		}
+
+		var targetDist = (worldPos - transform.position);
+		var targetDir = targetDist.normalized;
+
+		var targetHits = Physics.RaycastAll(transform.position, targetDir, targetDist.magnitude);;
+		foreach (var hit in targetHits) {
+			if (hit.rigidbody == ship.rigidBody) {
+				Stop();
+				return;
+			}
+		}
+
+		var targetRotation = Quaternion.LookRotation((Vector3)worldPos - transform.position);
+		if (targetRotation != transform.rotation) {
+		}
+		transform.rotation = targetRotation;
+		beam.startLifetime = Vector3.Distance(transform.position, worldPos) / Math.Abs(beam.startSpeed);
+		beam.enableEmission = true;
+		RaycastHit[] hits = Physics.SphereCastAll(transform.position, 0.20f, targetDir, targetDist.magnitude);
+		foreach (var hit in hits) {
+			var rigid = hit.collider.attachedRigidbody;
+			if (rigid != null) {
+				if (rigid != ship.rigidBody) {
+					rigid.AddForce(-targetDir * Math.Min(rigid.mass*2, Block.types["wall"].mass) * 100);
+					if (shieldCol != null)
+						Physics.IgnoreCollision(hit.collider, shieldCol);
+					hit.collider.attachedRigidbody.drag += 5;
+					captured.Add(hit.collider);
+				}
+			}
+		}		
+	}
+	
+	void Update() {
+		
+	}
+}
