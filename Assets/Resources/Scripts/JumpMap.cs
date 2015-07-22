5adf0727c3a698a236000cd42b5c77852e498eca
Jaiden Mispy <jaiden@mispy.me>
2015-07-22 19:34:19 +1000

    Make a start on sector jump map

---
+++ Assets/Resources/Scripts/JumpMap.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpMap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		for (var i = 0; i < 20; i++) {
+			var beacon = Pool.For("JumpBeacon").TakeObject();
+			var x = Random.Range(-100, 100);
+			var y = Random.Range(-100, 100);
+			beacon.transform.position = new Vector2(x, y);
+			beacon.SetActive(true);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
