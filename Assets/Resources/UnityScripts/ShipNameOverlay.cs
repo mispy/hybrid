112ebf25d8ed6206c8232ccfca44326491e730a0
Jaiden Mispy <jaiden@mispy.me>
2015-10-01 12:10:27 +1000

    Fix lag in rotation of ship name label

--- Assets/Resources/UnityScripts/ShipNameOverlay.cs
+++ Assets/Resources/UnityScripts/ShipNameOverlay.cs
@@ -7,14 +7,12 @@ public class ShipNameOverlay : MonoBehaviour {
     Text text;
     Canvas canvas;
 
-    // Use this for initialization
     void Start () {
         canvas = GetComponentInParent<Canvas>();
         ship = GetComponentInParent<Blockform>().ship;
         text = GetComponent<Text>();
     }
     
-    // Update is called once per frame
     void Update () {
         var friendlyColor = new Color(155/255f, 251/255f, 152/255f);
         var hostileColor = new Color(255/255f, 69/255f, 0f);
@@ -27,7 +25,7 @@ public class ShipNameOverlay : MonoBehaviour {
 
         text.fontSize = Mathf.CeilToInt(Mathf.Log(ship.blocks.size)*0.5f);
 
-        canvas.transform.rotation = Game.mainCamera.transform.rotation;
+        canvas.transform.rotation = Game.playerShip.form.transform.rotation;
         text.text = ship.name;
     }
 }
