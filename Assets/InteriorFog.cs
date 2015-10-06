aaaa88e688f68e341207685193ffd23e6862d954
Jaiden Mispy <jaiden@mispy.me>
2015-10-06 16:09:42 +1100

    Debounced and evented visibility updates

--- Assets/InteriorFog.cs
+++ Assets/InteriorFog.cs
@@ -5,23 +5,43 @@ using System.Linq;
 public class InteriorFog : MonoBehaviour {
 	Blockform form;
 	MeshRenderer meshRenderer;
+	Texture2D visibilityMap;
+	bool needsVisibilityUpdate;
 
 	// Use this for initialization
 	void Start () {
 		form = GetComponentInParent<Blockform>();
 		meshRenderer = GetComponent<MeshRenderer>();
+
+		form.blocks.OnBlockAdded += OnBlockUpdate;
+		form.blocks.OnBlockRemoved += OnBlockUpdate;
+
+		needsVisibilityUpdate = true;
+		InvokeRepeating("CheckForUpdate", 0f, 0.5f);
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	public void OnBlockUpdate(Block block) {
+		needsVisibilityUpdate = true;
+	}
+
+	void CheckForUpdate() {
+		if (needsVisibilityUpdate) {
+			UpdateVisibility();
+			needsVisibilityUpdate = false;
+		}
+	}
+
+	void UpdateVisibility() {
 		transform.localEulerAngles = new Vector3(90, 0, 0);
 		transform.localPosition = form.bounds.center;
 		transform.localScale = new Vector3(form.bounds.size.x, 0, form.bounds.size.y) / 10f;
 
-		var texture = new Texture2D(form.blocks.width, form.blocks.height);
-		texture.filterMode = FilterMode.Point;
-		var colors = new Color32[(form.blocks.width)*(form.blocks.height)];
+		var blocks = form.blocks;
 
+		var texture = new Texture2D(blocks.width, blocks.height);
+		texture.filterMode = FilterMode.Point;
+		var colors = new Color32[(blocks.width)*(blocks.height)];
+		
 		for (var i = 0; i < colors.Length; i++) {
 			if (form.ship == Game.playerShip) {
 				colors[i] = Color.clear;
@@ -29,8 +49,6 @@ public class InteriorFog : MonoBehaviour {
 				colors[i] = Color.black;
 			}
 		}
-
-		var blocks = form.blocks;
 
 		// Find an open space directly on the exterior
 		IntVector2 startPos = new IntVector2(0, 0);
@@ -43,20 +61,20 @@ public class InteriorFog : MonoBehaviour {
 				}
 			}
 		}
-
+		
 		foreach (var pos in BlockPather.Floodsight(blocks, startPos)) {
 			var x = pos.x - blocks.minX;
 			var y = pos.y - blocks.minY;
-
+			
 			if (x >= 0 && y >= 0 && x < blocks.width && y < blocks.height)
-				colors[y*form.blocks.width + x] = Color.clear;
+				colors[y*blocks.width + x] = Color.clear;
 		}
-
+		
 		texture.SetPixels32(colors);
 		texture.Apply();
-
+		
 		meshRenderer.material.SetTexture("_Visibility", texture);
 		meshRenderer.sortingLayerName = "Fog";
-		meshRenderer.material.mainTextureScale = new Vector2(form.bounds.size.x, form.bounds.size.y);
+		meshRenderer.material.mainTextureScale = new Vector2(form.bounds.size.x, form.bounds.size.y);			
 	}
 }
