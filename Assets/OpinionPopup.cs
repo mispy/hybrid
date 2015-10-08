11fdcd7283ad97e7fd12a4519c10e1a6b0bfdcf2
Jaiden Mispy <jaiden@mispy.me>
2015-10-08 20:06:46 +1100

    OpinionPopup

---
+++ Assets/OpinionPopup.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public enum OpinionEvent {
+	ShipRouted
+}
+
+public class OpinionPopup : MonoBehaviour {
+	Text text;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponentInChildren<Text>();
+	}
+
+	public void ApprovalChange(Faction faction, int amount) {
+		if (amount < 0) {
+			text.text += String.Format("\n<color=red>{1} {0} Disapproves</color>", faction.name, amount);
+		} else {
+			text.text += String.Format("\n<color=lime>+{1} {0} Approves</color>", faction.name, amount);
+		}
+	}
+
+	public void OnShipRouted(Ship ship) {
+		text.text += String.Format("\n<size=16><color=cyan>{0} - Routed</color></size>", ship.form.name);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
