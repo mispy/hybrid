5237bb2149429b9001354933d0ff20ffc70d0518
Jaiden Mispy <jaiden@mispy.me>
2015-10-21 07:05:35 +1100

    Add FPS counter

---
+++ Assets/FPSCounter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+
+public class FPSCounter : MonoBehaviour {
+    Text text;
+    float deltaTime = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	    text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        text.text = String.Format("{0} FPS", Mathf.FloorToInt(1.0f / deltaTime));
+	}
+}
